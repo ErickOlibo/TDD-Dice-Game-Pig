143bd6d0034468372dbc2ac3c5175988fb31a057
Erick Olibo <r0879036@student.thomasmore.be>
2023-02-28 13:31:04 +0100

    few methods implemented

--- src/DiceGame/brain.py
+++ src/DiceGame/brain.py
@@ -1,6 +1,26 @@
-
+from enums import Difficulty
 
 class Brain:
-    """A sort of Intelligence as the engine for the game"""
+    """ A sort of Intelligence as the engine for the game
+        It is only available to the CPU during a Player vs CPU game
+    """
+    
+    def __init__(self):
+        self._level = Difficulty.EASY
+        self._strategy = self.hold_20
+        pass
+    
+    
+    def hold_20(self, turn_points):
+        #return Play or Hold
+        pass
+    
+
+    def hold_25(self, turn_points):
+        pass
+    
+
+    def race_pace(self):
+        pass
     
     
--- src/DiceGame/test_brain.py
+++ src/DiceGame/test_brain.py
@@ -1,12 +1,30 @@
 import unittest
+from enums import Difficulty
 from brain import Brain
 
 class TestBrain(unittest.TestCase):
     
+    # runs at the start of EVERY test method execution
+    def setUp(self):
+        self.ai = Brain()
+        self.points = 20
+        
     # instantiate a object brain
     def test_brain(self):
-        ai = Brain()
-        self.assertIsNotNone(ai)
+        self.assertIsNotNone(self.ai)
+        
+    
+    def test_hold_20(self):
+        
+        self.assertTrue(self.ai.hold_20(self.points))
+        pass
+    
+    def test_hold_25(self):
+        pass
+    
+    def test_race_pace(self):
+        pass
+
 
 
 if __name__ == '__main__':
