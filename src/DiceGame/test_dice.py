fa841a3dd92ea15a73d032a10ed58d2c382ad2ea
engNesta <robert.nuhu0002@stud.hkr.se>
2023-02-28 15:18:41 +0100

    Implemented test for player

--- src/DiceGame/test_dice.py
+++ src/DiceGame/test_dice.py
@@ -1,11 +1,11 @@
 import unittest
-import DiceGame.dice as dice
+import dice 
 
 class TestDice(unittest.TestCase):
     def test_roll(self):
-        d = dice.Dice()
-        roll = d.roll()
-        self.assertTrue(1 <= roll <=6)
+        d = dice.Dice()                 #Instantiate the dice class
+        roll = d.roll()                 #roll the dice
+        self.assertTrue(1 <= roll <=6)  #Assert that the rolled dice value is between 1 ansd 6
 
 
 if __name__ == 'main':
