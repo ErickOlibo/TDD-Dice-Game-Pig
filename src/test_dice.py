157d3c17254908f95c4c98f7d2a2c4df27a2b601
engNesta <robert.nuhu0002@stud.hkr.se>
2023-02-28 13:38:26 +0100

    Added Test case for Dice class.

---
+++ src/test_dice.py
@@ -0,0 +1,13 @@
+import unittest
+import dice
+
+class TestDice(unittest.TestCase):
+    def test_roll(self):
+        d = dice.Dice()
+        roll = d.roll()
+        self.assertTrue(1 <= roll <=6)
+
+
+if __name__ == 'main':
+    unittest.main()
+        
