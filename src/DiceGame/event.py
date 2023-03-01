e522df7014b8ba1f8c927613c0e5a9f554eea4db
ErickOlibo <erick.olibo@gmail.com>
2023-03-01 11:25:10 +0100

    class to collect history of hands during game

---
+++ src/DiceGame/event.py
@@ -0,0 +1,27 @@
+
+class Event:
+    """ There is an event when a player has the hand in the game
+        Once the ID and Name are set, they cannot be changed
+    
+    """
+    
+    def __init__(self, id, name):
+        self._id = id
+        self._nane = name
+        self._points = 0
+        self._rolls = []
+        pass
+    
+    
+    def count_points(self):
+        sum = 0
+        for numb in self._rolls:
+            if numb == 1:
+                sum = 0
+                break
+        self._points = sum
+
+
+    def add_roll(self, value):
+        self._rolls.append(value)
+        
