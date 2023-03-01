e94fc64734c21a1379cefbf78f75982b2d67111d
ErickOlibo <erick.olibo@gmail.com>
2023-03-01 01:39:08 +0100

    added two properties (score and point during turn

--- src/DiceGame/player.py
+++ src/DiceGame/player.py
@@ -1,6 +1,8 @@
 class Player:
     def __init__(self, name):
         self.name = name
+        self.score = 0 # Needed in my brain implementation
+        self.turn_points = 0 # each player is aware of his/her hand running points
 
     def set_name(self, name):
         self.name = name
