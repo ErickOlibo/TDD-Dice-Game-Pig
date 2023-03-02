050fb2c4d7577138acec266c22db653cb8f6d3b2
ErickOlibo <erick.olibo@gmail.com>
2023-03-02 13:01:43 +0100

    starting the game logic

---
+++ src/DiceGame/main.py
@@ -0,0 +1,30 @@
+""" Main for running the Dice Game Pig
+    This contains the main program entry point
+"""
+
+import sys
+from game import Game
+
+
+def main():
+    flag = 2
+    while True:
+        game = Game()
+    # Show Menu of option
+    # - Mode selection (type of game)
+    # - Reload suspended Game
+    # - Show Highscore
+    
+    # Select the Mode
+    
+    # Enter Players Details (depending of mode)
+    
+        if flag == 0:
+            break
+
+
+
+
+
+if __name__ == "__main__":
+    main()
