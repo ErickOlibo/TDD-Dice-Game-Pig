44effbc666ce7caa764f08b4a550102f5cde1ad6
ErickOlibo <erick.olibo@gmail.com>
2023-03-02 15:49:04 +0100

    changing get_name to proprety getter/setter

--- src/DiceGame/player.py
+++ src/DiceGame/player.py
@@ -1,11 +1,20 @@
 class Player:
     def __init__(self, name):
-        self.name = name
+        self._name = name
         self.score = 0 # Needed in my brain implementation
         self.turn_points = 0 # each player is aware of his/her hand running points
 
-    def set_name(self, name):
-        self.name = name
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, name):
+        self._name = name
+        
+        
+    # def set_name(self, name):
+    #     self.name = name
 
-    def get_name(self):
-        return self.name
+    # def get_name(self):
+    #     return self.name
