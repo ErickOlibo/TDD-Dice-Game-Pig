21785a32f644848acd6c6835d7e0aa7101a1ee48
ErickOlibo <erick.olibo@gmail.com>
2023-03-03 19:38:44 +0100

    erased a line

--- src/DiceGame/test_player.py
+++ src/DiceGame/test_player.py
@@ -6,7 +6,6 @@ class TestPlayer(unittest.TestCase):
     def test_initialisedlayer(self):
         newPlayer = player.Player("Robert")              #Intantiate player and give it a name of Robert
         newPlayer.name = "Robert"
-        nameP = newPlayer.name
         self.assertEqual(newPlayer.get_name(), "Robert") #Assert that the name instantiated inside is Robert
         newPlayer.set_name("Marco")                      #Set a new name to the player
         self.assertEqual(newPlayer.get_name(), "Marco")  #Assert that the name is Marco
