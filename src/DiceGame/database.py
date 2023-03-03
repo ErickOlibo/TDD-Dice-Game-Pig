7494c5333c7fb0c397f771a13ae2efe798646ad6
ErickOlibo <erick.olibo@gmail.com>
2023-03-03 19:37:05 +0100

    creation of the database class and test

---
+++ src/DiceGame/database.py
@@ -0,0 +1,18 @@
+import pickle
+
+
+class Database:
+    
+    
+    #   1 - Load Games from the Games_DB
+    #       -> Games are unique by the UUID or name. 
+    #       -> The data structure should be a Dictionary 
+    #           * Keys: UUID (names)
+    #           * Values: Game objects
+    
+    #   2 - Winners List in the database Winners_DB
+    #       -> list of Tuple (Winner name, score, loser name, game_name) - order will de 
+    #       -> Generate Highscore_List from winners list()
+    #           * 
+   
+    pass
---
+++ src/DiceGame/test_database.py
@@ -0,0 +1,7 @@
+import unittest
+from database import Database
+
+class TestDatabase(unittest.TestCase):
+    
+    def setUp(self):
+        pass
