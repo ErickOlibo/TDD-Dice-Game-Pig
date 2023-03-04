de64e3a92e8c104d63b5f091f6ea82437bd4389b
ErickOlibo <erick.olibo@gmail.com>
2023-03-05 00:46:31 +0100

    Winner Class and test: OK

---
+++ src/DiceGame/test_winner.py
@@ -0,0 +1,24 @@
+import unittest
+from winner import Winner
+
+class TestWinner(unittest.TestCase):
+    
+    def setUp(self):
+        self.winner1 = Winner('Erick', 'CPU', 104)
+        self.winner2 = Winner('Robert', 'Erick', 101)
+        self.winner3 = Winner('CPU', 'Robert', 102)
+        self.winner4 = Winner('CPU', 'Erick', 105)
+    
+
+    def test_winner(self):
+        winner = Winner('CPU', 'Steve', 100)
+        self.assertIsNotNone(winner)
+    
+    
+    def test_get_name_score(self):
+        winners = [self.winner1, self.winner2, self.winner3, self.winner4]
+        names_scores = [winner.get_name_score for winner in winners]
+        self.assertEqual(len(names_scores), 4)
+    
+if __name__ == '__main__':
+    unittest.main()
