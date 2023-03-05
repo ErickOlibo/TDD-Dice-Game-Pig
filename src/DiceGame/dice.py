2982fb0ce7f5409472aadcf0dbb8d0c1983aa764
ErickOlibo <erick.olibo@gmail.com>
2023-03-05 19:09:32 +0100

    refactored the Dice class

--- src/DiceGame/dice.py
+++ src/DiceGame/dice.py
@@ -2,58 +2,43 @@ import random
 from helpers import Mode
 class Dice:
     """
-    Represents a dice that can be rolled to produce a random value from 1 to 6.
-
-    Attributes:
-        value (int): The value of the last roll.
-    """
-    def __init__(self):
-        self.value = None
-
-    def roll(self):
-        """
-        Rolls the dice and returns the result.
-
-        Returns:
-            int: The value of the roll.
-        """
-        self.value = random.randint(1, 6)
-        return self.value
-
-class CPUDice:
-    """
-    Represents a dice that the computer can use in the game.
-    The probability of rolling 1 depends on the game mode.
+    [Balanced Dice]
+        A dice represents a six-sided die that produces randam value 
+        from 1 to 6. When the mode = None the probability distribution of each 
+        size is 1/6Meaning the change of rolling a 1 is 1/6 = 16.7%. 
+        This is the only setting for a user.
+    
+    [Unbalanced Dice]
+        Mode is only define for the CPU as player. The probability of rolling 1 
+        depends on the game mode. There are 4 distinct modes:
+            - Easy: probability of 1 is [16.7%]
+            - Medium: probability of 1 is [9.1%]
+            - Hard: probability of 1 is [5.5%]
+            - Merciless: probability of 1 is [2.6%]
 
     Attributes:
         mode (Mode): The game mode that affects the probability of rolling 1.
-    """   
-    def __init__(self, mode: Mode) -> None:
-        """
-        Initializes the CPUDice with the given mode.
-
-        Args:
-            mode (Mode): The game mode that affects the probability of rolling 1.
-
-        Raises:
-            TypeError: If the mode is not of type Mode.
-            ValueError: If the mode is DUEL, which does not have a CPU Dice.
-        """
-        if not isinstance(mode, Mode):
-            raise TypeError('mode must be of type Mode!')
-        
-        if mode == Mode.DUEL:
-            raise ValueError('The DUEL mode does not have a CPU Dice!')
+    """
+    def __init__(self, mode: Mode = None):
+        self._mode = mode
         
         r1 = list(range(1,7))
         r2 = list(range(2,7))
         r3 = list(range(3,7))
         r4 = list(range(4,7))
-        self._mode = mode
-        self._easy = r1  # same probability of rolling 1 as player [16.7%]
-        self._medium = r1 + r2  # probability of rolling 1 is [9.1%]
-        self._hard = r1 + r2 + r3 + r4  # probability of rolling 1 is [5.5%]
-        self._merciless = r1 + r2 + r2 + r3 + r3 + r3 + r3 + r4 + r4  #  [2.6%]
+        self._easy = r1
+        self._medium = r1 + r2
+        self._hard = r1 + r2 + r3 + r4
+        self._merciless = r1 + r2 + r2 + r3 + r3 + r3 + r3 + r4 + r4
+        
+        
+    def roll(self) -> int:
+        if self._mode == None: 
+            return random.randint(1, 6)
+        
+        if self._mode in Mode:
+            return random.choice(self._items())
+        
 
     @property
     def mode(self) -> Mode:
@@ -75,28 +60,22 @@ class CPUDice:
 
         Raises:
             TypeError: If the mode is not of type Mode.
+            ValueError: If mode is set to DUEL
         """
-        if not isinstance(mode, Mode):
+        if not isinstance(mode, Mode) and mode is not None:
             raise TypeError('mode must be of type Mode!')
+        if mode == Mode.DUEL:
+            raise ValueError('The DUEL mode does not have a special!')
         self._mode = mode
-    
 
-    def roll(self) -> int:
-        """
-        Rolls the dice and returns the result, using the current game mode.
-
-        Returns:
-            int: The value of the roll.
-        """
-        return random.choice(self._items())
-    
 
     def _items(self) -> list:
         """
-        Returns a list of possible values for the dice, based on the current game mode.
+        Returns a list of unbalanced distribution of dice-sizes. Depending on
+        the mode, the probability of rolling a 1 goes from 16.7% to 2.6%
 
         Returns:
-            list: A list of possible values for the dice.
+            list: A list integers from 1 to 6 with unbalanced distribution.
         """
         items = None
         if self._mode is Mode.SOLO_EASY: items = self._easy
--- src/DiceGame/test_dice.py
+++ src/DiceGame/test_dice.py
@@ -1,55 +1,68 @@
 import unittest
-from dice import Dice, CPUDice
+from dice import Dice
 from helpers import Mode
 
 class TestDice(unittest.TestCase):
-    def test_roll(self):
-        d = Dice()
-        roll = d.roll()
-        self.assertTrue(1 <= roll <=6)
-
-
-class TestCPUDice(unittest.TestCase):
-    
     def setUp(self):
-        self.cpu_dice = CPUDice(Mode.SOLO_EASY)
+        self.dice = Dice()
     
-    # instantiate an object CPUDice
-    def test_cpudice(self):
-        self.assertIsNotNone(self.cpu_dice)
+    
+    def test_dice(self):
+        die = Dice()
+        self.assertIsNotNone(die)
+        die2 = Dice(Mode.SOLO_MEDIUM)
+        self.assertNotEqual(die, die2)
+    
+    
+    def test_mode(self):
+        self.dice.mode = Mode.SOLO_MERCILESS
+        self.assertEqual(self.dice.mode, Mode.SOLO_MERCILESS)
+        self.assertIn(self.dice.mode, Mode)
         
-        # Test error raised
         with self.assertRaises(TypeError):
-            self.cpu_dice = CPUDice('Mode.EASY')
+            self.dice.mode = 'SOLO_MERCILESS'
         
         with self.assertRaises(ValueError):
-            self.cpu_dice = CPUDice(Mode.DUEL)
-        
-
-    def test_roll(self):
-        self.cpu_dice.mode = Mode.SOLO_HARD
-        self.assertIsInstance(self.cpu_dice.roll(), int)
-        self.assertIn(self.cpu_dice.roll(), range(1,7))
-        
+            self.dice.mode = Mode.DUEL
     
+    
+    def test_roll(self):
+        # Balanced Dice
+        dice = Dice()
+        self.assertIn(dice.roll(), range(1, 7))
+        
+        # Unbalanced Dice
+        size = 10000
+        proba = [0.167, 0.091, 0.055, 0.026] # probabilities of rolling 1
+        modes = [Mode.SOLO_EASY, Mode.SOLO_MEDIUM, Mode.SOLO_HARD, Mode.SOLO_MERCILESS]
+        for i in range(len(proba)):
+            rolls = 0
+            dice.mode = modes[i]
+            for j in range(size):
+                if dice.roll() == 1:
+                    rolls += 1
+            self.assertAlmostEqual(rolls/size, proba[i],2)
+
+        
     def test__items(self):
-        self.cpu_dice.mode = Mode.SOLO_EASY
-        list_items = self.cpu_dice._items()
+        dice = Dice()
+        dice.mode = Mode.SOLO_EASY
+        list_items = dice._items()
         self.assertEqual(len(list_items), 6)
         
-        self.cpu_dice.mode = Mode.SOLO_MEDIUM
-        list_items = self.cpu_dice._items()
+        dice.mode = Mode.SOLO_MEDIUM
+        list_items = dice._items()
         self.assertEqual(len(list_items), 11)
         
-        self.cpu_dice.mode = Mode.SOLO_HARD
-        list_items = self.cpu_dice._items()
+        dice.mode = Mode.SOLO_HARD
+        list_items = dice._items()
         self.assertEqual(len(list_items), 18)
         
-        self.cpu_dice.mode = Mode.SOLO_MERCILESS
-        list_items = self.cpu_dice._items()
+        dice.mode = Mode.SOLO_MERCILESS
+        list_items = dice._items()
         self.assertEqual(len(list_items), 38)
-        
-        
+
+
 
 
 if __name__ == '__main__':
