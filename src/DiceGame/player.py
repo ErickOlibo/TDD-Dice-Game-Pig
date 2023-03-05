6395edf6a46a47baff9934c6957e64cca6fba97b
ErickOlibo <erick.olibo@gmail.com>
2023-03-05 15:50:12 +0100

    Removed cpu.py and updated player.py

--- src/DiceGame/player.py
+++ src/DiceGame/player.py
@@ -1,20 +1,44 @@
+from brain import Brain
+from helpers import Turn
+
 class Player:
-    def __init__(self, name):
-        self.name = name
-        self.score = 0       # Needed in my brain implementation
-        self.turn_points = 0 # each player is aware of his/her hand running points
+    def __init__(self, name: str, brain: Brain = None):
+        self._name = name
+        self._brain = brain
+        self._score = 0
+
 
     @property
-    def name(self):
+    def name(self) -> str:
         return self._name
     
     @name.setter
-    def name(self, name):
+    def name(self, name: str):
         self._name = name
-        
-        
-    # def set_name(self, name):
-    #     self.name = name
 
-    # def get_name(self):
-    #     return self.name
+
+    @property
+    def brain(self) -> Brain:
+        return self._brain
+    
+    @brain.setter
+    def brain(self, brain: Brain):
+        if not isinstance(brain, Brain):
+            raise TypeError('brain must be of instance Brain!')
+        self._brain = brain
+    
+    
+    @property
+    def score(self) -> int:
+        return self._score
+    
+    
+    def playing_choice(self, score, turn_points) -> Turn:
+        if self._brain != None:
+            return self._brain.action(score, turn_points)
+        else:
+            return None
+    
+    
+    def add_points_to_score(self, points: int):
+        self._score += points
--- src/DiceGame/test_player.py
+++ src/DiceGame/test_player.py
@@ -1,14 +1,56 @@
 import unittest
-import player
+from player import Player
+from helpers import Turn, Tactic
+from brain import Brain
 
 
 class TestPlayer(unittest.TestCase):
-    def test_initialisedlayer(self):
-        newPlayer = player.Player("Robert")              #Intantiate player and give it a name of Robert
-        newPlayer.name = "Robert"
-        self.assertEqual(newPlayer.get_name(), "Robert") #Assert that the name instantiated inside is Robert
-        newPlayer.set_name("Marco")                      #Set a new name to the player
-        self.assertEqual(newPlayer.get_name(), "Marco")  #Assert that the name is Marco
+    
+    def setUp(self):
+        self.jen = Player('Jennifer')
+        pass
+    
+    def test_player(self):
+        erick = Player('Erick')
+        self.assertIsNotNone(erick)
+        cpu = Player('CPU', Brain())
+        self.assertIsNotNone(cpu)
+        self.assertEqual(cpu.score, 0)
+    
+    
+    def test_name(self):
+        previous = self.jen.name
+        self.jen.name = 'Oliver'
+        self.assertNotEqual(self.jen.name, previous)
+    
+    
+    def test_brain(self):
+        with self.assertRaises(TypeError):
+            self.jen.brain = 'Brain'
+        
+        self.jen.brain = Brain()
+        self.assertIsInstance(self.jen.brain, Brain)
+    
+    
+    def test_score(self):
+        self.assertEqual(self.jen.score, 0)        
+    
+    
+    def test_playing_choice(self):
+        self.assertIsNone(self.jen.playing_choice(45, 5))
+        cpu = Player('CPU', Brain())
+        self.assertIn(cpu.playing_choice(35,10), Turn)
 
+    
+    def test_add_points_to_score(self):
+        self.jen.add_points_to_score(23)
+        self.assertEqual(self.jen.score, 23)
+        self.jen.add_points_to_score(17)
+        self.assertNotEqual(self.jen.score, 23)
+        pass
+    
+
+if __name__ == '__main__':
+    unittest.main()
 
     
