b4f46818413795e58e201966346bb174ba049ee9
ErickOlibo <erick.olibo@gmail.com>
2023-03-05 03:09:01 +0100

    refactor and reviewd logic

--- src/DiceGame/game.py
+++ src/DiceGame/game.py
@@ -3,6 +3,7 @@ from helpers import Textual, Mode
 from player import Player
 from event import Event
 from winner import Winner
+import time
 class Game:
     """ 
         A instance of a game played. It has all the rules, 
@@ -13,28 +14,25 @@ class Game:
 
     
     def __init__(self):
-        self._name = str(uuid.uuid4()) # initial name as universal unique ID
-        self._rules = Textual.RULES.value
-        self._menu = Textual.MENU.value
-        self._mode = None
-        self._player_one = None
-        self._player_two = None
-        self._participants = None
-        self._hand = None # 
-        self._histogram = [] # List of Events. The order is guarantee by Python
-        self._state = False # (Save and Quit) OR Quit without Saving
+        self._time_stamp = round(time.time())  # sorting Games chronologically
+        self._codename = None # Only for game in a suspended state
+        self._mode = Mode.SOLO_EASY  # default
+        self._participants = [] # Can be 2 players or 1 player and cpu
+        self._hand = None # (name of the player)
+        self._histogram = list[Event] # List of Events. The order is guarantee by Python
+        self._is_game_over = False # set once a winner is declared
         self._winner = None  # object of type Winner
     
     
     @property
-    def name(self) -> str:
-        return self._name
+    def winner(self) -> Winner:
+        return self._winner
     
-    @name.setter
-    def name(self, name: str):
-        if not isinstance(name, str):
+    @winner.setter
+    def winner(self, winner: Winner):
+        if not isinstance(winner, Winner):
             raise TypeError('name must be a string!')
-        self._name = name
+        self._winner= winner
         
     @property
     def mode(self) -> Mode:
@@ -45,8 +43,7 @@ class Game:
         if not isinstance(mode, Mode):
             raise TypeError('Mode must be SOLO or DUEL!')
         self._mode = mode
-        # HERE SHOULD 
-        
+
 
     def display_rules(self):
         print(self._rules)
