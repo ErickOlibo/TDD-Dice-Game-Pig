d9f89a6e63848820364a307bd0bcde892e7354de
ErickOlibo <erick.olibo@gmail.com>
2023-03-05 03:10:03 +0100

    continuing the logic of the game in main

--- src/DiceGame/main.py
+++ src/DiceGame/main.py
@@ -4,12 +4,48 @@
 
 import sys
 from game import Game
-from helpers import Mode
+from helpers import Mode, Entry_Menu
+from database import Database
+from gui import GUI
 
 
 def main():
+    ### INITIALISATION  ONLY ONCE ###
+    # A - Load Games from Database
+    # B - Extract HighScore from Database
+    db = Database()
+    gui = GUI()
+    games_dict = db.get_games()
+    highscore = db.get_highscore()
     flag = 2
+    
     while True:
+
+        # 1 - Show the entry_menu (New Game - Resume Game)
+        entry_choice = gui.display_entry_menu()
+
+        # 2 - deal with the entry_choice for the right option
+        if entry_choice == Entry_Menu.NEW.value:
+            print('Choice is New Game')
+        
+        if entry_choice == Entry_Menu.RESUME.value:
+            is_in_database = False
+            while not is_in_database:
+                game_code = input('Enter the Game Code (or B to go back):')
+                if  game_code.lower == 'b':
+                    
+                # ask for the Game code to resume
+                    break
+            
+            print('Choice is Resume Game')
+        
+        if entry_choice == Entry_Menu.EXIT.value:
+            print('Choice is Exit Main')
+        
+        #   1 - New Game()
+        #   2 - Resume Gane()
+        # get the list of code and games from the database
+        
         # start a new instance of game
         game = Game()
         
@@ -24,7 +60,7 @@ def main():
         # 5 - solo merciless
         # RESUME GAME
         # 6 - restart (get game code [change name to code])
-        # HIGHTLIGHT
+        # HIGHLIGHTS
         # 7 - Show Highscore
         
     # Show Menu of option
