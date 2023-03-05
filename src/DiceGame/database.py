bc7c5d7ed32786e8cc9445a5ccaa216d6a57c0b8
ErickOlibo <erick.olibo@gmail.com>
2023-03-05 03:08:11 +0100

    Database refactoring and new logic

--- src/DiceGame/database.py
+++ src/DiceGame/database.py
@@ -1,18 +1,50 @@
 import pickle
+from game import Game
 
 
 class Database:
     
+    def __init__(self):
+        self._games = self._load_games_db()
+        self._highscore = None # array 
+        self._paused_games = None
     
-    #   1 - Load Games from the Games_DB
-    #       -> Games are unique by the UUID or name. 
-    #       -> The data structure should be a Dictionary 
-    #           * Keys: UUID (names)
-    #           * Values: Game objects
-    
-    #   2 - Winners List in the database Winners_DB
-    #       -> list of Tuple (Winner name, score, loser name, game_name) - order will de 
-    #       -> Generate Highscore_List from winners list()
-    #           * 
+
    
-    pass
+    def _load_games_db(self) -> list[Game]:
+        # unpickle database
+        
+        # Update properties
+        self._update_properties
+        return []
+
+
+    def _update_properties(self):
+        pass
+    
+
+    def get_highscore(self) -> list:
+        winners = [game.winner for game in self._games]
+        return self._highscore
+    
+    
+    def save_game(self, game: Game):
+        # add game to list of games
+        self._games.append(game)
+        self._update_properties()
+        # update
+        
+        pass
+    
+    
+    
+    
+    def update_highscrore(self):
+        pass
+    
+    def _generate_highscore(self):
+        # get high score from list of finished games
+        pass
+    
+    def _generate_used_codenames(self):
+        pass
--- src/DiceGame/test_database.py
+++ src/DiceGame/test_database.py
@@ -1,7 +1,35 @@
 import unittest
 from database import Database
+from game import Game
+from helpers import Textual, Mode
 
 class TestDatabase(unittest.TestCase):
     
     def setUp(self):
-        pass
+        self.db = Database()
+        self.game1 = Game()
+        self.game2 = Game()
+        self.game3 = Game()
+        self.games = [self.game1, self.game2, self.game3]
+    
+    
+    def test_name(self):
+        uuids = [game._uuid for game in self.games]
+        self.assertEqual(len(set(uuids)), len(uuids)) # unique uuid
+        for game in self.games:
+            self.assertEqual(game._rules, Textual.RULES.value)
+            self.assertEqual(game._menu, Textual.MENU.value)
+            self.assertIsNotNone(game._uuid)
+
+
+    def test_mode(self):
+        self.assertEqual(self.game1.mode, Mode.SOLO_EASY)
+        self.game2.mode = Mode.SOLO_MERCILESS
+        self.assertNotEqual(self.game1.mode, self.game2.mode)
+
+
+
+        
+
+if __name__ == '__main__':
+    unittest.main()
