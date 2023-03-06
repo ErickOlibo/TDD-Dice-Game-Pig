4fdd56409bb97139c1d97db123d7a752732c7152
ErickOlibo <erick.olibo@gmail.com>
2023-03-06 08:31:06 +0100

    Reversed To previous version

--- src/DiceGame/player.py
+++ src/DiceGame/player.py
@@ -3,29 +3,33 @@ from dice import Dice
 from helpers import Turn
 
 class Player:
-    """A class representing a player in a game.
-
-    Attributes:
-        name (str): The name of the player.
-        score (int): The total score of the player.
-        turn_points (int): The points accumulated by the player in the current turn.
-
     """
+    A Player is a participant to the game. There are 2 types:
+        - user: played by an person through the keyboard input
+        - cpu: played internally by the computer
+    Depending on the type, few properties are available
+    
+    Attributes:
+        name: the name of the player.
+        brain (Brain): This brain is available only to the computer to similate
+            an sort of intelligent behaviour
+        dice (Dice): each player rolls with their own dice. A user has a balanced
+            dice, while the computer has an unbalanced one
+    
+    """
+    def __init__(self, name: str, brain: Brain = None, dice: Dice = Dice()):
+        self._name = name
+        self._brain = brain
+        self._dice = dice
+        self._score = 0
 
-    def __init__(self, name):
-        """Initializes a new player with the given name."""
-        self.name = name
-        self.score = 0       # Needed in my brain implementation
-        self.turn_points = 0 # each player is aware of his/her hand running points
 
     @property
-    def name(self):
-        """Returns the player's name."""
+    def name(self) -> str:
         return self._name
     
     @name.setter
-    def name(self, name):
-        """Sets the player's name to the given value."""
+    def name(self, name: str):
         self._name = name
 
 
