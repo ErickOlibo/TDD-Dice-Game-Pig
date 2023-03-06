d5dfdfe9b3b88b63129bd962d124401089ede440
ErickOlibo <erick.olibo@gmail.com>
2023-03-06 03:53:37 +0100

    improving the database class

--- src/DiceGame/database.py
+++ src/DiceGame/database.py
@@ -1,17 +1,54 @@
 import pickle
+import random
 from game import Game
-
+from winner import Winner
 
 class Database:
     
     def __init__(self):
-        self._games = self._load_games_db()
-        self._highscore = None # array 
-        self._paused_games = None
+        # dummy list of winners for testing
+        dummy_winners = []
+        size = 500
+        people = ['Erick', 'Robert', 'Jennifer', 'Ciara', 'CPU']
+        for _ in range(size):
+            couple = random.sample(people, k=2)
+            points = random.randint(100, 105)
+            stamp = random.randint(20000, 50000)
+            dummy_winners.append(Winner(couple[0], couple[1], points, stamp))
+        data = [win.data for win in dummy_winners]
+        #print(data)
+        data.sort(key=lambda win: win[0])
+        collection = []
+        name = data[0][1]
+        pts = data[0][3]
+        streak = 1
+        for i, win in enumerate(data, 1):
+            victor = win[1]
+            score = win[3]
+            if victor == name:
+                pts += score
+                streak += 1
+            else:
+                collection.append([name, streak, pts])
+                name = victor
+                pts = score
+                streak = 1
+            
+        #[print(w) for w in collection]
+        self._highscore = collection
+        #[print(item) for item in data]
+        #[print(win.data) for win in self._winners]
+
+        # Unpickle database and set paused game & winners
+
+        #self._paused_games = self._load_paused_games_db() # to change -> from pickle
+        #self._winners = self._load_winners() # to change -> from pickle
+        #self._highscore = None # array 
+        #self._paused_games = None
     
 
    
-    def _load_games_db(self) -> list[Game]:
+    def _load_paused_games_db(self) -> list[Game]:
         # unpickle database
         
         # Update properties
@@ -19,25 +56,27 @@ class Database:
         return []
 
 
-    def _update_properties(self):
+    def _load_winners(self) -> list[Winner]:
         pass
+    
+    def add_winner(self, winner: Winner):
+        self._winners.append(winner)
     
 
     def get_highscore(self) -> list:
-        winners = [game.winner for game in self._games]
         return self._highscore
     
     
-    def save_game(self, game: Game):
-        # add game to list of games
-        self._games.append(game)
-        self._update_properties()
-        # update
-        
+    def save_paused_game(self, game: Game):
+        self._paused_games.append(game)
+        self._update_paused_game_database()
+
+    
+    def _update_paused_game_database(self):
         pass
     
-    
-    
+    def get_saved_game(self, codename: str) -> Game:
+        pass
     
     def update_highscrore(self):
         pass
