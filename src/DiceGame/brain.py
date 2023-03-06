28061f822b6bde8005f4c7cc8ffefc3a562898aa
engNesta <robert.nuhu0002@stud.hkr.se>
2023-03-06 07:25:28 +0100

    Updated player class with docstrings

--- src/DiceGame/brain.py
+++ src/DiceGame/brain.py
@@ -2,9 +2,28 @@ import random
 from helpers import  Turn, Tactic
 
 class Brain:
-    
-    """ A sort of Intelligence for the Player when it represent the Computer
-        This brain is only givento players of type CPU
+    """
+    A sort of intelligence for the computer-controlled player in the game.
+
+    Attributes:
+    -----------
+    _strategy: Tactic
+        The current tactic being used by the computer player.
+    _current_turns: int
+        The number of turns the computer player has taken in a round.
+    _max_turns: int
+        The maximum number of turns a computer player can take in a round.
+    _target: int
+        The target score for the game.
+
+    Methods:
+    --------
+    _random_strategy() -> Tactic:
+        Private method that returns a random tactic from the Tactic enum.
+
+    action(score: int, turn_points: int) -> Turn:
+        Public method that determines the next move for the computer player.
+
     """
     def __init__(self):
         self._strategy = self._random_strategy()
@@ -14,12 +33,29 @@ class Brain:
 
 
     def _random_strategy(self) -> Tactic:
+        """
+        Returns a random tactic from the Tactic enum.
+        """
         return random.choice(list(Tactic))
         
         
     # Actions are defined for the Brain playing as the CPU
     def action(self, score, turn_points) -> Turn:
+        """
+        Determines the next move for the computer player based on its current strategy and the game state.
 
+        Parameters:
+        -----------
+        score: int
+            The current score of the computer player.
+        turn_points: int
+            The number of points the computer player has accumulated in the current turn.
+
+        Returns:
+        --------
+        Turn
+            The next move for the computer player (Hold or Roll).
+        """
         if self._strategy in [Tactic.TEN, Tactic.TWENTY, Tactic.TWENTY_FIVE]:
             return Turn.HOLD if turn_points >= self._strategy.value else Turn.ROLL
         
