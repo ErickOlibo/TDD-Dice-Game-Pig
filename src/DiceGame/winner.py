ae0b257d5efad32c44dac0acfd34b168a57906c1
ErickOlibo <erick.olibo@gmail.com>
2023-03-06 03:55:55 +0100

    optional time stamp in method call

--- src/DiceGame/winner.py
+++ src/DiceGame/winner.py
@@ -4,8 +4,8 @@ class Winner:
     """ This object will serve as a data structure to save game's winner stats
     """
     
-    def __init__(self, winner: str, loser: str, score: int):
-        self._time_stamp = round(time.time())  # sorting Winners chronologically
+    def __init__(self, winner: str, loser: str, score: int, stamp = round(time.time())):
+        self._time_stamp = stamp  # sorting Winners chronologically
         self._winner = winner
         self._loser = loser
         self._score = score
