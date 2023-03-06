c620bbf5cfadea55cb9068c846b101c399b21bd5
ErickOlibo <erick.olibo@gmail.com>
2023-03-06 03:52:54 +0100

    testing the random sample

--- practice-testing/testing.py
+++ practice-testing/testing.py
@@ -1,10 +1,13 @@
-
+import random
 
 # for i in range(9400, 10000):
 #     print(f'{i}) - {chr(i)}')
 
-str = '│  ● ┌─  └─  ─┘  ─┐'
+# str = '│  ● ┌─  └─  ─┘  ─┐'
 
-print('\n\n')
-for c in str:
-    print(f'{c}) - {ord(c)}')
+# print('\n\n')
+# for c in str:
+#     print(f'{c}) - {ord(c)}')
+
+people = ['Erick', 'Robert', 'Jennifer', 'Ciara', 'CPU']
+print(random.sample(people, 2))
