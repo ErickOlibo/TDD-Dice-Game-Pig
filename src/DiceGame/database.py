4b87556575d8d60b30ff59236d0a83e6d96e94a9
ErickOlibo <erick.olibo@gmail.com>
2023-03-07 15:23:48 +0100

    rewrote logic of Storing & Retrieving Data

--- src/DiceGame/database.py
+++ src/DiceGame/database.py
@@ -2,29 +2,77 @@ import pickle
 import random
 from game import Game
 from winner import Winner
+from helpers import Data_Path as PATH, CODE_NAMES
+import time
+import os
+os.chdir(os.path.dirname(os.path.realpath(__file__)))
 
 class Database:
     
     def __init__(self):
-        # dummy list of winners for testing
-        dummy_winners = []
-        size = 500
-        people = ['Erick', 'Robert', 'Jennifer', 'Ciara', 'CPU']
-        for _ in range(size):
-            couple = random.sample(people, k=2)
-            points = random.randint(100, 105)
-            stamp = random.randint(20000, 50000)
-            dummy_winners.append(Winner(couple[0], couple[1], points, stamp))
-        data = [win.data for win in dummy_winners]
-        #print(data)
+        self._games: dict[str : Game] # paused games
+        self._winners = self._load_data(PATH.WINNERS)
+        self._highscore = self._generate_highscore()
+
+
+
+    @property
+    def highscore(self) -> list:
+        return self._highscore
+    
+    
+    
+    ## PUBLIC METHODS
+    def load_game(self, code: str) -> Game:
+        game = self._games.pop(code, None)
+        if game != None: self._store_data(self._games, PATH.GAMES)
+        return game
+    
+    
+    def store_game(self, game: Game) -> str:
+        used_codes = self._games.keys()
+        while code := random.choice(CODE_NAMES) not in used_codes:
+            break
+        self._games[code] = game
+        self._store_data(self._games, PATH.GAMES)
+        return code
+    
+    
+    def add_winner(self, winner: Winner):
+        self._winners.append(winner)
+        self._store_data(self._winners, PATH.WINNERS)
+        self._update_highscore()
+        
+    
+    
+    ## PRIVATE METHODS
+    def _update_highscore(self):
+        self._highscore = self._generate_highscore()
+        pass
+        
+    def _load_data(self, p: PATH):
+        file = open(p.value, 'rb')
+        data = pickle.load(file)
+        file.close()
+        return data
+    
+    
+    def _store_data(self, data, p: PATH):
+        file = open(p.value, 'wb')
+        pickle.dump(data, file)
+        file.close()
+
+
+    def _generate_highscore(self) -> list:
+        data = [win.data for win in self._winners]
         data.sort(key=lambda win: win[0])
         collection = []
         name = data[0][1]
-        pts = data[0][3]
+        pts = data[0][2]
         streak = 1
-        for i, win in enumerate(data, 1):
+        for _, win in enumerate(data, 1):
             victor = win[1]
-            score = win[3]
+            score = win[2]
             if victor == name:
                 pts += score
                 streak += 1
@@ -33,57 +81,18 @@ class Database:
                 name = victor
                 pts = score
                 streak = 1
-            
-        #[print(w) for w in collection]
-        self._highscore = collection
-        #[print(item) for item in data]
-        #[print(win.data) for win in self._winners]
-
-        # Unpickle database and set paused game & winners
-
-        #self._paused_games = self._load_paused_games_db() # to change -> from pickle
-        #self._winners = self._load_winners() # to change -> from pickle
-        #self._highscore = None # array 
-        #self._paused_games = None
-    
-
-   
-    def _load_paused_games_db(self) -> list[Game]:
-        # unpickle database
-        
-        # Update properties
-        self._update_properties
-        return []
+        collection.sort(key=lambda row: (-row[1], -row[2]))
+        return collection
 
 
-    def _load_winners(self) -> list[Winner]:
-        pass
+    def _load_dummy_winners(self) -> list[Winner]:
+        dummies = []
+        size = 10
+        people = ['Erick', 'Robert', 'Jennifer', 'Ciara', 'CPU']
+        for _ in range(size):
+            winner = random.choice(people)
+            points = random.randint(100, 105)
+            stamp = random.randint(20000, 50000)
+            dummies.append(Winner(winner, points, stamp))
+        return dummies
     
-    def add_winner(self, winner: Winner):
-        self._winners.append(winner)
-    
-
-    def get_highscore(self) -> list:
-        return self._highscore
-    
-    
-    def save_paused_game(self, game: Game):
-        self._paused_games.append(game)
-        self._update_paused_game_database()
-
-    
-    def _update_paused_game_database(self):
-        pass
-    
-    def get_saved_game(self, codename: str) -> Game:
-        pass
-    
-    def update_highscrore(self):
-        pass
-    
-    def _generate_highscore(self):
-        # get high score from list of finished games
-        pass
-    
-    def _generate_used_codenames(self):
-        pass
