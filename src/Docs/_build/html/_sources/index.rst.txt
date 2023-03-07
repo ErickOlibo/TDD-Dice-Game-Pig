72dc88acb512ccaeeeb62e781075a226862f5501
engNesta <robert.nuhu0002@stud.hkr.se>
2023-03-07 10:55:30 +0100

    Created Docs page

---
+++ src/Docs/_build/html/_sources/index.rst.txt
@@ -0,0 +1,20 @@
+.. TDD-Dice-Game-Pig documentation master file, created by
+   sphinx-quickstart on Mon Mar  6 14:15:38 2023.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to TDD-Dice-Game-Pig's documentation!
+=============================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   modules
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
---
+++ src/Docs/conf.py
@@ -0,0 +1,33 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+import os
+import sys
+#import sphinx_rtd_theme
+
+sys.path.append(os.path.abspath(".."))
+
+project = 'TDD-Dice-Game-Pig'
+copyright = '2023, Erick Olibo, Robert Nesta Nuhu'
+author = 'Erick Olibo, Robert Nesta Nuhu'
+release = '1.0.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ["sphinx.ext.todo", "sphinx.ext.viewcode", "sphinx.ext.autodoc"]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
