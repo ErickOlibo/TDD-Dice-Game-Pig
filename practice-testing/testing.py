d9bb1b43244aad302d309e74f08358a4c66b4569
ErickOlibo <erick.olibo@gmail.com>
2023-03-08 04:25:10 +0100

    just testing few ideas

--- practice-testing/testing.py
+++ practice-testing/testing.py
@@ -1,19 +1,42 @@
 import random
+import time
 
-rolls = [1,2,3,4,5,6,7,8,9,10]
-ck = 3
-#myLists = zip([rolls[i::ck] for i in range(ck)])
-#print(myLists)
 
-aList = []
-# for i in range(0, len(rolls), ck):
-#     print(i)
-#     print(rolls)
-#     print(rolls[i:ck+i])
-#     aList.append(rolls[i:ck+i])
+print('Enter your name: ', end='')
+time.sleep(1)
+print('Erick')
+
+
+# for line in sys.stdin:
+#     if 'q' == line.rstrip():
+#         break
+#     print(f'Input : {line}')
+
+# print('Exit')
+
+
+
+
+
+
+
+
+
+
+# rolls = [1,2,3,4,5,6,7,8,9,10]
+# ck = 3
+# #myLists = zip([rolls[i::ck] for i in range(ck)])
+# #print(myLists)
+
+# aList = []
+# # for i in range(0, len(rolls), ck):
+# #     print(i)
+# #     print(rolls)
+# #     print(rolls[i:ck+i])
+# #     aList.append(rolls[i:ck+i])
     
-# print(aList)
+# # print(aList)
 
-roll_split = [rolls[i:ck+i] for i in range(0, len(rolls), ck)]
+# roll_split = [rolls[i:ck+i] for i in range(0, len(rolls), ck)]
 
-print(roll_split)
+# print(roll_split)
