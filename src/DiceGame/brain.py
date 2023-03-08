e1075cb7efdb4cdf30bbe7b5ab783508557a9e92
ErickOlibo <erick.olibo@gmail.com>
2023-03-08 19:49:00 +0100

    cleaning brain.py with flak8

--- src/DiceGame/brain.py
+++ src/DiceGame/brain.py
@@ -1,5 +1,7 @@
+"""This class creates the illution of intelligence for the Computer."""
 import random
-from helpers import  Turn, Tactic
+from helpers import Turn, Tactic
+
 
 class Brain:
     """
@@ -7,59 +9,45 @@ class Brain:
 
     Attributes:
     -----------
-    _strategy: Tactic
-        The current tactic being used by the computer player.
-    _current_turns: int
-        The number of turns the computer player has taken in a round.
-    _max_turns: int
-        The maximum number of turns a computer player can take in a round.
-    _target: int
-        The target score for the game.
+    No public attibutes
 
     Methods:
     --------
-    _random_strategy() -> Tactic:
-        Private method that returns a random tactic from the Tactic enum.
-
     action(score: int, turn_points: int) -> Turn:
         Public method that determines the next move for the computer player.
-
     """
+
     def __init__(self):
+        """Construct the necessary attributes for the brain object."""
         self._strategy = self._random_strategy()
         self._current_turns = 0
         self._max_turns = 4
         self._target = 100
 
-
     def _random_strategy(self) -> Tactic:
-        """
-        Returns a random tactic from the Tactic enum.
-        """
-        # random.choice(list(Tactic))
-        return random.choice([Tactic.TWENTY, Tactic.TWENTY_FIVE, Tactic.FOUR_TURNS])
-        
-        
-    # Actions are defined for the Brain playing as the CPU
+        return random.choice([Tactic.TWENTY, Tactic.TWENTY_FIVE,
+                              Tactic.FOUR_TURNS])
+
     def action(self, score: int, turn_points: int) -> Turn:
-        """
-        Determines the next move for the computer player based on its current strategy and the game state.
+        """Return the action to take based on strategy and mode.
 
         Parameters:
         -----------
         score: int
             The current score of the computer player.
         turn_points: int
-            The number of points the computer player has accumulated in the current turn.
+            The number of points the computer player has accumulated in the
+            current turn.
 
         Returns:
         --------
-        Turn
+        Turn(Enum)
             The next move for the computer player (Hold or Roll).
         """
         if self._strategy in [Tactic.TEN, Tactic.TWENTY, Tactic.TWENTY_FIVE]:
-            return Turn.HOLD if turn_points >= self._strategy.value else Turn.ROLL
-        
+            threshold = turn_points >= self._strategy.value
+            return Turn.HOLD if threshold else Turn.ROLL
+
         if self._strategy == Tactic.FOUR_TURNS:
             if self._current_turns == 0:
                 if turn_points >= 30:
@@ -71,19 +59,10 @@ class Brain:
                 remain = (self._max_turns - self._current_turns)
                 if remain == 0:
                     raise ZeroDivisionError('Cannot divid by Zero!')
-                
+
                 threshold = (self._target - score) // remain
                 if turn_points >= threshold:
-                    self._current_turns +=1
+                    self._current_turns += 1
                     return Turn.HOLD
                 else:
                     return Turn.ROLL
-        
-        # if self._strategy == Tactic.FOUR_TURNS:
-        #     if self._current_turns == 0:
-        #         if turn_points >= 35:
-        #             self._current_turns += 1
-        #             return Turn.HOLD
-        #         else:
-        #             return Turn.ROLL
-            
