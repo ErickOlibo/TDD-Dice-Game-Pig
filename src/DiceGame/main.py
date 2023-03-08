226298f23eb4b356ef89ac0b6006b5be1acd9bab
ErickOlibo <erick.olibo@gmail.com>
2023-03-08 22:36:41 +0100

    Flake done!

--- src/DiceGame/main.py
+++ src/DiceGame/main.py
@@ -1,75 +1,52 @@
-""" Main for running the Dice Game Pig
-    This contains the main program entry point
-"""
-
-import sys
+"""This file is the entry point of the program."""
 from game import Game
-from helpers import *
+from helpers import Start_Up, Mode
 from database import Database
-from gui import GUI
-import time
 
 
 def main():
-    # db = Database()
-    # game = Game(db)
-    # game.training_game()
-
-    
-    # A GAME PLAYED gives you a Winner object or a codename for Database
+    """Control the flow of the application."""
     while True:
-        #break
         # 1 - Start an instance of Game
         db = Database()
         game = Game(db)
-        
-        
+
         # 2 - Get the option from the user for the Start Up menu
         start_choice = game.show_menu('START UP', Start_Up.MENU)
-        
+
         # 3 - Handle Chosen Option
-        game.menu_transition() # Clean Terminal Display
-        if start_choice == Start_Up.EXIT: 
-            break 
-        
+        game.menu_transition()
+        if start_choice == Start_Up.EXIT:
+            break
+
         if start_choice == Start_Up.NEW_GAME:
             new_game_choice = game.show_menu('NEW GAME MODE', Mode.MENU)
             if new_game_choice == Mode.BACK:
                 game.menu_transition()
                 continue
-            
+
             if new_game_choice == Mode.DUEL:
                 game.set_duel_players()
                 game.play()
-            
-            solo_mode = [mode for mode in Mode if mode not in [Mode.DUEL, Mode.BACK]]
+
+            solo_mode = [
+                mode for mode in Mode if mode not in [Mode.DUEL, Mode.BACK]
+                ]
             if new_game_choice in solo_mode:
                 game.set_solo_player(new_game_choice)
                 game.play()
 
-            # START THE NEW GAME
-            #game.play()
-                
         if start_choice == Start_Up.RESUME_GAME:
-            codename = game.request_codename_from_user() # CHANGE THIS METHOD NAME 
+            codename = game.request_codename_from_user()
             game.play(codename)
-        
-        
+
         if start_choice == Start_Up.HIGH_SCORE:
             game.show_highscore(db.highscore)
             game.press_any_keys_to_continue()
 
-
         if start_choice == Start_Up.RULES:
             game.display_rules()
             game.press_any_keys_to_continue()
-            
-
-
-
-
-
-
 
 
 if __name__ == "__main__":
