9cf5e5a504086295bb9c6ea6cabc04047a122d8d
ErickOlibo <erick.olibo@gmail.com>
2023-03-08 22:49:12 +0100

    Flake done for non docstring issues

--- src/DiceGame/player.py
+++ src/DiceGame/player.py
@@ -1,22 +1,25 @@
+"""This module manages the characteristics of the player object."""
 from brain import Brain
 from dice import Dice
 from helpers import Turn
 
+
 class Player:
-    """
-    A Player is a participant to the game. There are 2 types:
-        - user: played by an person through the keyboard input
-        - cpu: played internally by the computer
-    Depending on the type, few properties are available
-    
+    """A Player is a participant to the game of Pig.
+
+    There are 2 types:
+        - user: played by an person through the keyboard input.
+        - cpu: played internally by the computer.
+    The qualities of the dice and brain differs.
+
     Attributes:
         name: the name of the player.
-        brain (Brain): This brain is available only to the computer to similate
-            an sort of intelligent behaviour
-        dice (Dice): each player rolls with their own dice. A user has a balanced
-            dice, while the computer has an unbalanced one
-    
+        brain (Brain): This brain is available only to the computer to
+        similate a sort of intelligent behaviour.
+        dice (Dice): each player rolls with their own dice.
+        A user has a balanced dice, while the CPU has a weigthed one.
     """
+
     def __init__(self, name: str, brain: Brain = None, dice: Dice = Dice()):
         self._name = name
         self._brain = brain
@@ -24,46 +27,41 @@ class Player:
         self._score = 0
         self._rolls = []
 
-
     @property
     def name(self) -> str:
         return self._name
-    
+
     @name.setter
     def name(self, name: str):
         self._name = name
 
-
     @property
     def brain(self) -> Brain:
         return self._brain
-    
+
     @brain.setter
     def brain(self, brain: Brain):
         if not isinstance(brain, Brain):
             raise TypeError('brain must be of instance Brain!')
         self._brain = brain
-    
-    
+
     @property
     def score(self) -> int:
         return self._score
-    
+
     @property
     def rolls(self) -> list[int]:
         return self._rolls
-    
-    
+
     def playing_choice(self, score, turn_points) -> Turn:
-        if self._brain != None:
+        if self._brain is not None:
             return self._brain.action(score, turn_points)
         else:
             return None
-    
-    
+
     def add_points_to_score(self, points: int):
         self._score += points
-        
+
     def roll_dice(self) -> int:
         roll = self._dice.roll()
         self._rolls.append(roll)
